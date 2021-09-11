01672badf2b726463a5c70df1bdc28a3e618a900
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-09-11 16:21:04 -0400

    some css changes

--- Album.js
+++ Album.js
@@ -8,43 +8,74 @@ class Album {
 
     }
 
-    // static addAlbum(album) {
-    //     new Album(album)
-    // }
-
-    renderCard = () => {
-        const { name, artist, genre, image, description, id} = this.data
-        document.getElementById("album-container").innerHTML += `
-        <div class="album-card" data-id=${id}>
-        <img src="${image}"alt="${name}"/>
-        <p class="labels">${name}</p>
-        <p class="labels">${artist}</p>
-        <p class="labels">${genre}</p>
-        <p>${description}</p>
-        </div>`
-    }
-
-    static handleIndexClick = (e) => {
-
-        if (e.target.tagName == "IMG" || e.target.classList.contains("labels")) {
-            console.log(e.target)
-        }
-    }
-
-    static renderAlbumIndex = ()  => {
-        const albumContainer = document.createElement("div")
-        albumContainer.id = "album-container"
-        document.getElementById("main").appendChild(albumContainer)
-        this.all.forEach(album => album.renderCard())
-
-        albumContainer.addEventListener("click", this.handleIndexClick)
-    }
-
     static getAlbums = () => {
         api.getAlbums().then(albums => {
             albums.forEach(album => new Album(album))
             this.renderAlbumIndex()
         })
     }
+
+
+    renderCard = () => {
+        const { name, artist, genre, image, description, id} = this.data
+        document.getElementById("album-container").innerHTML += `
+        <div class="album-card" data-id=${id}>
+            <div id="header" class="insta-header">
+            <span class="insta-actions">...</span>
+            <div class"album-name">
+            ${name}
+            </div>
+            <span class="profile-name">username</span>
+            </div>
+                <div class="image">
+                    <img src="${image}"alt="${name}"/>
+                </div>
+            <div id="artist-genre">
+            <p class="labels">${artist}</p>
+            <p class="labels">${genre}</p>
+            </div>
+            <div id="description"
+            <p>${description}</p>
+            </div>
+        </div>`
+    }
+
+    static find = (id) => this.all.find(album => album.data.id == id )
+
+    
+    static renderAlbumIndex = ()  => {
+        const main = document.getElementById("main")
+        main.innerHTML = ""
+        const albumContainer = document.createElement("div")
+        albumContainer.id = "album-container"
+        main.appendChild(albumContainer)
+        this.all.forEach(album => album.renderCard())
+
+        albumContainer.addEventListener("click", this.handleIndexClick)
+    }
+
+    static handleIndexClick = (e) => {
+
+        if (e.target.tagName == "IMG" || e.target.classList.contains("labels")) {
+            const id = e.target.closest(".album-card").dataset.id
+            this.find(id).renderShow()
+        }
+    }
+
+    renderShow = () => {
+        const { name, artist, genre, image, description, id} = this.data
+        document.getElementById("main").innerHTML = ` 
+        <div class="show">
+        <h1>${name}</h1>
+        <img src="${image}"alt="${name}"/>
+        <p class="labels">${artist}</p>
+        <p class="labels">${genre}</p>
+        <p>${description}</p>
+        <button id="goBack"> Go Back </button>
+        </div>`
+        document.getElementById("goBack").addEventListener("click", Album.renderAlbumIndex)
+    }
+
+  
 
 }
